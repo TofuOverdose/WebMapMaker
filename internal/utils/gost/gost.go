75a8b4783b886ff083fe6c4eecdba45699ffc3ee
Sergey Grishanin <s.grishanin.wrk@gmail.com>
2020-08-03 00:07:37 +0700

    Add print methods for gost

--- internal/utils/gost/gost.go
+++ internal/utils/gost/gost.go
@@ -92,6 +92,9 @@ func overwriteBlank(data []byte) []byte {
 	return append(data, whitespace...)
 }
 
+// Write writes len(data) bytes to the File.
+// It returns the number of bytes written and an error, if any.
+// Write returns a non-nil error when n != len(b).
 func (sb *StatusBar) Write(data []byte) (int, error) {
 	output := []byte(fmt.Sprintf("\r%s\n", overwriteBlank(data)))
 	n, err := os.Stdout.Write([]byte(output))
@@ -105,6 +108,32 @@ func (sb *StatusBar) Write(data []byte) (int, error) {
 	}
 
 	return n, nil
+}
+
+// WriteString is like Write, but writes the contents of string s rather than
+// a slice of bytes.
+func (sb StatusBar) WriteString(s string) (ret int, err error) {
+	return sb.Write([]byte(s))
+}
+
+// Print formats using the default formats for its operands and writes to standard output.
+// Spaces are added between operands when neither is a string.
+// It returns the number of bytes written and any write error encountered.
+func (sb StatusBar) Print(a ...interface{}) (n int, err error) {
+	return sb.WriteString(fmt.Sprint(a...))
+}
+
+// Println formats using the default formats for its operands and writes to standard output.
+// Spaces are always added between operands and a newline is appended.
+// It returns the number of bytes written and any write error encountered.
+func (sb StatusBar) Println(a ...interface{}) (n int, err error) {
+	return sb.WriteString(fmt.Sprintln(a...))
+}
+
+// Printf formats according to a format specifier and writes to standard output.
+// It returns the number of bytes written and any write error encountered.
+func (sb StatusBar) Printf(format string, a ...interface{}) (n int, err error) {
+	return sb.WriteString(fmt.Sprintf(format, a...))
 }
 
 func (sb *StatusBar) Close() {
